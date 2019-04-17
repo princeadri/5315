7717e90fb71c8326937e9f281869018bf8549e9e
cess <adrineda@seattleu.edu>
2019-04-16 19:10:27 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,94 +1,124 @@
 {
   "data": [
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "Best Time to Visit": "October to December",
-      "7-Day Budget": "$3,750"
+      "name": "James Bond",
+      "designation": "Accountant",
+      "salary": "$170,750",
+      "joining_date": "2011/07/25",
+      "office": "Tokyo",
+      "extension": "8422"
     },
     {
-      "Country": "Egypt",
-      "Attractions": "Pyramids,tombs,Sahara desert",
-      "Best Time to Visit": "October to December",
-      "7-Day Budget": "$3,500"
+      "name": "Brielle Williamson",
+      "designation": "Integration Specialist",
+      "salary": "$372,000",
+      "joining_date": "2012/12/02",
+      "office": "New York",
+      "extension": "4804"
     },
     {
-      "Country": "France",
-      "Attractions": "Louvre, Eiffel Tower",
-      "Best Time to Visit": "September to November",
-      "7-Day Budget": "$6,000"
+      "name": "Ashton Cox",
+      "designation": "Junior Technical Author",
+      "salary": "$86,000",
+      "joining_date": "2009/01/12",
+      "office": "San Francisco",
+      "extension": "1562"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Airi Satou",
+      "designation": "Accountant",
+      "salary": "$162,700",
+      "joining_date": "2008/11/28",
+      "office": "Tokyo",
+      "extension": "5407"
     },
     {
-      "Country": "Iceland",
-      "Attractions": "Aurora borealis, blue lagoon",
-      "Best Time to Visit": "September to March",
-      "7-Day Budget": "$4,500"
+      "name": "Caesar Vance",
+      "designation": "Pre-Sales Support",
+      "salary": "$106,450",
+      "joining_date": "2011/12/12",
+      "office": "New York",
+      "extension": "8330"
     },
     {
-      "Country": "Japan",
-      "Attractions": "Mt. Fuji, temples, hot springs, A5 wagyu beef",
-      "Best Time to Visit": "March to May",
-      "7-Day Budget": "$4,500"
+      "name": "Shad Decker",
+      "designation": "Regional Director",
+      "salary": "$183,000",
+      "joining_date": "2008/11/13",
+      "office": "Edinburgh",
+      "extension": "6373"
     },
     {
-      "Country": "Jerusalem",
-      "Attractions": "Holy land tour",
-      "Best Time to Visit": "April to May",
-      "7-Day Budget": "$4,000"
+      "name": "Cedric Kelly",
+      "designation": "Senior Javascript Developer",
+      "salary": "$433,060",
+      "joining_date": "2012/03/29",
+      "office": "Edinburgh",
+      "extension": "6224"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Haley Kennedy",
+      "designation": "Senior Marketing Designer",
+      "salary": "$313,500",
+      "joining_date": "2012/12/18",
+      "office": "London",
+      "extension": "3597"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Colleen Hurst",
+      "designation": "Javascript Developer",
+      "salary": "$205,500",
+      "joining_date": "2009/09/15",
+      "office": "San Francisco",
+      "extension": "2360"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Dai Rios",
+      "designation": "Personnel Lead",
+      "salary": "$217,500",
+      "joining_date": "2012/09/26",
+      "office": "Edinburgh",
+      "extension": "2290"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Herrod Chandler",
+      "designation": "Sales Assistant",
+      "salary": "$137,500",
+      "joining_date": "2012/08/06",
+      "office": "San Francisco",
+      "extension": "9608"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Rhona Davidson",
+      "designation": "Integration Specialist",
+      "salary": "$327,900",
+      "joining_date": "2010/10/14",
+      "office": "Tokyo",
+      "extension": "6200"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Sonya Frost",
+      "designation": "Software Engineer",
+      "salary": "$103,600",
+      "joining_date": "2008/12/13",
+      "office": "Edinburgh",
+      "extension": "1667"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Jena Gaines",
+      "designation": "Office Manager",
+      "salary": "$90,560",
+      "joining_date": "2008/12/19",
+      "office": "London",
+      "extension": "3814"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "name": "Quinn Flynn",
+      "designation": "Support Lead",
+      "salary": "$342,000",
+      "joining_date": "2013/03/03",
+      "office": "Edinburgh",
+      "extension": "9497"
     }
   ]
 }
