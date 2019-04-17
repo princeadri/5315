53f85e9c466a52d00461556508fb406818d871d8
cess <adrineda@seattleu.edu>
2019-04-16 19:57:08 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -4,91 +4,121 @@
       "Country": "Chile",
       "Attractions": "Easter Island",
       "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "Best Time to Visit": "October to December",
+      "office": "Tokyo",
+      "7 Day Budget": "$4000"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Brielle Williamson",
+      "designation": "Integration Specialist",
+      "salary": "$372,000",
+      "joining_date": "2012/12/02",
+      "office": "New York",
+      "extension": "4804"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Ashton Cox",
+      "designation": "Junior Technical Author",
+      "salary": "$86,000",
+      "joining_date": "2009/01/12",
+      "office": "San Francisco",
+      "extension": "1562"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Airi Satou",
+      "designation": "Accountant",
+      "salary": "$162,700",
+      "joining_date": "2008/11/28",
+      "office": "Tokyo",
+      "extension": "5407"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Caesar Vance",
+      "designation": "Pre-Sales Support",
+      "salary": "$106,450",
+      "joining_date": "2011/12/12",
+      "office": "New York",
+      "extension": "8330"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Shad Decker",
+      "designation": "Regional Director",
+      "salary": "$183,000",
+      "joining_date": "2008/11/13",
+      "office": "Edinburgh",
+      "extension": "6373"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Cedric Kelly",
+      "designation": "Senior Javascript Developer",
+      "salary": "$433,060",
+      "joining_date": "2012/03/29",
+      "office": "Edinburgh",
+      "extension": "6224"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Haley Kennedy",
+      "designation": "Senior Marketing Designer",
+      "salary": "$313,500",
+      "joining_date": "2012/12/18",
+      "office": "London",
+      "extension": "3597"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Colleen Hurst",
+      "designation": "Javascript Developer",
+      "salary": "$205,500",
+      "joining_date": "2009/09/15",
+      "office": "San Francisco",
+      "extension": "2360"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Dai Rios",
+      "designation": "Personnel Lead",
+      "salary": "$217,500",
+      "joining_date": "2012/09/26",
+      "office": "Edinburgh",
+      "extension": "2290"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Herrod Chandler",
+      "designation": "Sales Assistant",
+      "salary": "$137,500",
+      "joining_date": "2012/08/06",
+      "office": "San Francisco",
+      "extension": "9608"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Rhona Davidson",
+      "designation": "Integration Specialist",
+      "salary": "$327,900",
+      "joining_date": "2010/10/14",
+      "office": "Tokyo",
+      "extension": "6200"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Sonya Frost",
+      "designation": "Software Engineer",
+      "salary": "$103,600",
+      "joining_date": "2008/12/13",
+      "office": "Edinburgh",
+      "extension": "1667"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Jena Gaines",
+      "designation": "Office Manager",
+      "salary": "$90,560",
+      "joining_date": "2008/12/19",
+      "office": "London",
+      "extension": "3814"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7_Day_Budget": "$4000",
-      "Best_Time_to_Visit": "October to December"
+      "name": "Quinn Flynn",
+      "designation": "Support Lead",
+      "salary": "$342,000",
+      "joining_date": "2013/03/03",
+      "office": "Edinburgh",
+      "extension": "9497"
     }
   ]
 }
