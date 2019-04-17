35e27f26f44452e22ff46cf691005e63809c47bd
cess <adrineda@seattleu.edu>
2019-04-16 20:16:01 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -3,18 +3,18 @@
     {
       "Country": "Chile",
       "Attractions": "Easter Island",
+      "7 Day Budget": "$4,000",
+      "Best Time to Visit": "October to December"
+    },
+    {
+      "Country": "Egypt",
+      "Attractions": "Pyramids, tombs, Sahara Desert",
       "7 Day Budget": "$4,500",
       "Best Time to Visit": "October to December"
     },
     {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
-      "7 Day Budget": "$4,500",
-      "Best Time to Visit": "October to December"
-    },
-    {
-      "Country": "Chile",
-      "Attractions": "Easter Island",
+      "Country": "France",
+      "Attractions": "Louvre, Eiffel Tower",
       "7 Day Budget": "$4,500",
       "Best Time to Visit": "October to December"
     },
