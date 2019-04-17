bf3fa0bf74e7b8d78feaa50192d81a82eb3a466e
cess <adrineda@seattleu.edu>
2019-04-16 19:23:07 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,12 +1,10 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "country": "Chile",
+      "attractions": "Easter Island",
+      "7_Day_Budget": "$4,000",
+      "best_time_to_visit": "October to December"
     },
     {
       "name": "Brielle Williamson",
