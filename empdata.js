cdc7d31b44fb79f300c4afeda88bf469735f58f9
cess <adrineda@seattleu.edu>
2019-04-16 19:07:39 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -4,7 +4,7 @@
       "Country": "Chile",
       "Attractions": "Easter Island",
       "Best Time to Visit": "October to December",
-      "7-Day Budget": "$4,000"
+      "7-Day Budget": "$3,750"
     },
     {
       "Country": "Egypt",
@@ -25,22 +25,22 @@
       "7-Day Budget": "$4,500"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
+      "Country": "Iceland",
+      "Attractions": "Aurora borealis, blue lagoon",
+      "Best Time to Visit": "September to March",
       "7-Day Budget": "$4,500"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
+      "Country": "Japan",
+      "Attractions": "Mt. Fuji, temples, hot springs, A5 wagyu beef",
+      "Best Time to Visit": "March to May",
       "7-Day Budget": "$4,500"
     },
     {
-      "Country": "Greece",
-      "Attractions": "Parthenon, Meteora, Melissani cave",
-      "Best Time to Visit": "September to October",
-      "7-Day Budget": "$4,500"
+      "Country": "Jerusalem",
+      "Attractions": "Holy land tour",
+      "Best Time to Visit": "April to May",
+      "7-Day Budget": "$4,000"
     },
     {
       "Country": "Greece",
